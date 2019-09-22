e50df7e7e76be6d70ee415239d8c75593de4b078
DeLbFuH <45275322+DeLbFuH@users.noreply.github.com>
2019-09-22 19:08:04 +0300

    Залил весь проект

---
+++ Turbokyrsa4/Render.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Turbokyrsa4
+{
+    public class Render
+    {
+        Bitmap map = new Bitmap(1280, 960);
+        Graphics graph;
+
+        public Bitmap RenderMap()
+        {
+            graph = Graphics.FromImage(map);
+            Image globalmap = Image.FromFile("MAP V1.jpg");
+            graph.DrawImage(globalmap, 1, 1, 1279, 959);
+            return map;
+        }
+
+        public void FindTowns()
+        {
+
+        }
+    }
+}
